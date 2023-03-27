cd0cd5571f01683de4f29a902405c576094d1ee3
ShrivatsaMishra <55681172+ShrivatsaMishra@users.noreply.github.com>
2023-03-27 22:50:48 +0530

    Added directories and dependancies for spatial audio

--- app/src/main/java/com/androchef/cameraxfacedetection/MainActivity.kt
+++ app/src/main/java/com/androchef/cameraxfacedetection/MainActivity.kt
@@ -3,15 +3,20 @@ package com.androchef.cameraxfacedetection
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.androchef.cameraxfacedetection.camerax.CameraManager
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.vr.sdk.audio.GvrAudioEngine
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var cameraManager: CameraManager
+    private lateinit var gvrAudioEngine: GvrAudioEngine
+    private var soundId: Int = GvrAudioEngine.INVALID_ID
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,17 @@ class MainActivity : AppCompatActivity() {
         createCameraManager()
         checkForPermission()
         onClicks()
+        val gvrAudioEngine = GvrAudioEngine(this, GvrAudioEngine.RenderingMode.BINAURAL_HIGH_QUALITY)
+
+        val objectSoundFile = "cube_sound.wav"
+        Log.i(">>>", "Working Directory = " + System.getProperty("user.dir"))
+        gvrAudioEngine.preloadSoundFile(objectSoundFile)
+        val sourceId = gvrAudioEngine.createSoundObject(objectSoundFile)
+        Log.i(">>>", "" + sourceId)
+        gvrAudioEngine.setSoundObjectPosition(sourceId, 0f, 10f, 0f)
+        gvrAudioEngine.setHeadRotation(0f, 0f, 0f, 0f)
+        gvrAudioEngine.setHeadPosition(0f, 0f, 0f)
+        gvrAudioEngine.playSound(sourceId, true)
     }
 
     private fun checkForPermission() {
